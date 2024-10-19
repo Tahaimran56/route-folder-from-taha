95b67db23ec1c7ad18990d47ed8a6fc8a955f63e
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2024-10-19 17:42:46 +0500

    Add files via upload
    hello everyone!
    i done my first route work 
    i made folder called contact and given endpoint (page.tsx)
    first start with home page and now  /contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact(){
+    return ( 
+        <div>
+                    <h1>contact information</h1>
+                  <p>this page is route folder </p>
+        </div>
+    )
+}
